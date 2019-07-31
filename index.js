8e9f0b003fed93180abf63d4e3fc9002fa61f905
Ed Abrahamsen <esabrahamsen@yahoo.com>
2019-07-31 13:20:31 -0700

    hw lambda

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-var time = require('time');
-exports.handler = (event, context, callback) => {
-    var currentTime = new time.Date();
-    currentTime.setTimezone("America/Los_Angeles");
-    callback(null, {
-        statusCode: '200',
-        body: 'The time in Los Angeles is: ' + currentTime.toString(),
-    });
-};
+'use strict'
+
+postData('https://hooks.slack.com/services/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', {text: 'Hello World'})
+  .then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
+  .catch(error => console.error(error));
+
+function postData(url = '', data = {}) {
+  // Default options are marked with *
+    return fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, cors, *same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json',
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrer: 'no-referrer', // no-referrer, *client
+        body: JSON.stringify(data), // body data type must match "Content-Type" header
+    })
+    .then(response => response.json()); // parses JSON response into native JavaScript objects 
+}
